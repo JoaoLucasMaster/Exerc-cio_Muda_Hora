503fd5cae20774f4daabe994ede7d166e2654155
João Lucas N.D <84422552+JoaoLucasMaster@users.noreply.github.com>
2023-02-14 18:18:32 -0300

    Add files via upload

---
+++ ExercíciosJavascriptJoão/script.js
@@ -0,0 +1,6 @@
+function Carregamento_Dados(){
+    let mensagem = window.document.getElementById('mensagem')
+    let img = window.document.getElementById('img')
+    let data = new Date()
+    let hora = new data.getHours()
+}
