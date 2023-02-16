43a417c6ad070f33a40284085713535f54908d3d
João Lucas N.D <84422552+JoaoLucasMaster@users.noreply.github.com>
2023-02-16 00:45:02 -0300

    Delete ExercíciosJavascriptJoão directory

--- ExercíciosJavascriptJoão/script.js
+++
@@ -1,7 +0,0 @@
-function Carregamento(){
-    var  mensagem = window.document.getElementById('mensagem')
-    var img = window.document.getElementById('img')
-    var data = new Date()
-    var hora = new data.getHours()
-    mensagem.innerHTML=`Agora são ${hora} horas.`
-}   
